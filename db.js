83a41a456f9cf6e2b873e8221caddcd3a828902e
Eric P. Katz <ericpkatz@gmail.com>
2018-04-30 21:30:57 -0400

    commit

--- db.js
+++ db.js
@@ -13,7 +13,31 @@ const Image = conn.define('image', {
   },
 },{
   hooks: {
+    beforeUpdate: function(image){
+      //loop over new images or deletions
+      return Promise.all(
+        Object.keys(image.uris).reduce( (memo, key) => {
+          memo.push(image.uris[key] ? image.saveToCloud(key):image.removeFromCloud(key) );
+          return memo;
+        }, [])
+      )
+      .then( uploads => {
+        //merge with previous data
+        image.uris = uploads.reduce((memo, result)=> {
+          memo[result.key] = result.uri;
+          return memo;
+        }, image._previousDataValues.uris);
+        //remove null values
+        image.uris = Object.keys(image.uris).reduce((memo, key)=> {
+          if(image.uris[key]){
+            memo[key] = image.uris[key];
+          }
+          return memo;
+        }, {}); 
+      });
+    },
     beforeCreate: function(image){
+      //loop over images and upload for each image
       return Promise.all(
         Object.keys(image.uris).reduce( (memo, key) => {
           memo.push(image.saveToCloud(key));
@@ -21,8 +45,11 @@ const Image = conn.define('image', {
         }, [])
       )
       .then( uploads => {
+        //only set if image uri has been set
         image.uris = uploads.reduce((memo, result)=> {
-          memo[result.key] = result.uri;
+          if(result.uri){
+            memo[result.key] = result.uri;
+          }
           return memo;
         }, {});
       });
@@ -38,10 +65,15 @@ Image.prototype.saveToCloud = function(key){
       if(err){
         return reject(err);
       }
+      const data = this.uris[key];
+      const extensions = data.split(';')[0].split('/');
+      const extension = extensions[extensions.length - 1];
+      const Body = new Buffer(data.replace(/^data:image\/\w+;base64,/, ''), 'base64');
       S3.upload({
         Bucket: process.env.BUCKET,
         Key: key,
-        Body: this.uris[key],
+        Body: Body,
+        ContentType: `image/${extension}`,
         ACL: 'public-read',
       }, (err, data)=>{
         if(err){
@@ -51,7 +83,26 @@ Image.prototype.saveToCloud = function(key){
       });
     });
   });
+}
 
+Image.prototype.removeFromCloud = function(key){
+  return new Promise((resolve, reject)=> {
+    const S3 = new AWS.S3();
+    S3.createBucket({ Bucket: process.env.BUCKET}, (err, bucket)=>{
+      if(err){
+        return reject(err);
+      }
+      S3.deleteObject({
+        Bucket: process.env.BUCKET,
+        Key: key,
+      }, (err, data)=>{
+        if(err){
+          return reject(err);
+        }
+        resolve({uri: null, key });
+      });
+    });
+  });
 }
 
 const sync = ()=> conn.sync({ force: true });
--- db.spec.js
+++ db.spec.js
@@ -2,9 +2,12 @@ const expect = require('chai').expect;
 const db = require('./db');
 const { Image } = db.models;
 const smiley = require('./smiley');
+const frowny = require('./frowny');
 const uuid = require('uuid/v4');
+const AWS = require('aws-sdk');
 
 describe('Image model', ()=> {
+
   beforeEach(()=> {
     return db.sync();
   });
@@ -12,71 +15,126 @@ describe('Image model', ()=> {
     expect(Image).to.be.ok;
   });
   describe('unit tests', ()=> {
-    describe('both uploads are successful', ()=> {
-
-      it('an image can be created', ()=> {
-        const uris = {};
-        const key1 = uuid();
-        const key2 = uuid();
-        uris[key1] = smiley;
-        uris[key2] = smiley;
-        const image = Image.build({
-          uris
-        });
-        const imageUri = 'http://aws.com/image-uri';
-        const generateUriFromKey = (key)=>`${imageUri}-${key}`;
-        image.saveToCloud = (key)=> { 
-          const obj = {
-            key,
-            uri: generateUriFromKey(key)
-          };
-          return Promise.resolve(obj);
-        };
-        return image.save()
-          .then( image => {
-            expect(image.uris[key1]).to.equal(generateUriFromKey(key1));
-            expect(image.uris[key2]).to.equal(generateUriFromKey(key2));
+    describe('creating', ()=> {
+      describe('both uploads are successful', ()=> {
+        it('an image can be created', ()=> {
+          const uris = {};
+          const key1 = uuid();
+          const key2 = uuid();
+          const key3 = uuid();
+          uris[key1] = smiley;
+          uris[key2] = smiley;
+          const image = Image.build({
+            uris
           });
+          const imageUri = 'http://aws.com/image-uri';
+          const generateUriFromKey = (key)=>`${imageUri}-${key}`;
+          image.saveToCloud = (key)=> { 
+            const obj = {
+              key,
+              uri: generateUriFromKey(key)
+            };
+            return Promise.resolve(obj);
+          };
+          return image.save()
+            .then( image => {
+              expect(image.uris[key1]).to.equal(generateUriFromKey(key1));
+              expect(image.uris[key2]).to.equal(generateUriFromKey(key2));
+              image.uris = { };
+              image.uris[key2] = null;
+              image.uris[key3] = smiley;
+              image.removeFromCloud = (key)=> {
+                return Promise.resolve({ key, uri: null });
+
+              }
+              return image.save();
+            })
+            .then( image => {
+              expect(image.uris[key1]).to.equal(generateUriFromKey(key1));
+              expect(image.uris[key3]).to.equal(generateUriFromKey(key3));
+              expect(image.uris[key2]).to.not.be.ok;
+            });
+        });
       });
-    });
-
-    describe('one upload is successful out of two', ()=> {
-      it('an image can be created', ()=> {
-        const uris = {};
-        const key1 = uuid();
-        const key2 = uuid();
-        uris[key1] = smiley;
-        uris[key2] = smiley;
-        const image = Image.build({
-          uris
-        });
-        const imageUri = 'http://aws.com/image-uri';
-        const generateUriFromKey = (key)=>`${imageUri}-${key}`;
-        image.saveToCloud = (key)=> { 
-          const obj = {
-            key,
-            uri: key === key1 ? generateUriFromKey(key): null
-          };
-          return Promise.resolve(obj);
-        };
-        return image.save()
-          .then( image => {
-            expect(image.uris[key1]).to.equal(generateUriFromKey(key1));
-            expect(image.uris[key2]).to.equal(null);
+      describe('one upload is successful out of two', ()=> {
+        it('an image can be created', ()=> {
+          const uris = {};
+          const key1 = uuid();
+          const key2 = uuid();
+          uris[key1] = smiley;
+          uris[key2] = smiley;
+          const image = Image.build({
+            uris
           });
+          const imageUri = 'http://aws.com/image-uri';
+          const generateUriFromKey = (key)=>`${imageUri}-${key}`;
+          image.saveToCloud = (key)=> { 
+            const obj = {
+              key,
+              uri: key === key1 ? generateUriFromKey(key): null
+            };
+            return Promise.resolve(obj);
+          };
+          return image.save()
+            .then( image => {
+              expect(image.uris[key1]).to.equal(generateUriFromKey(key1));
+              expect(image.uris[key2]).not.to.be.ok;
+            });
+        });
       });
     });
   });
   describe('integration tests', ()=> {
+    /*
+    beforeEach(()=> {
+      return new Promise((resolve, reject)=> {
+        const S3 = new AWS.S3();
+        S3.listObjects({ Bucket: process.env.BUCKET}, (err, data)=>{
+          if(err){
+            return reject(err);
+          }
+          Promise.all(data.Contents.map( object => {
+            return new Promise((resolve, reject)=> {
+              S3.deleteObject({
+                Bucket: process.env.BUCKET,
+                Key: object.Key
+              }, (err, data)=> {
+                resolve();
+              })
+            });
+          }))
+          .then(()=> {
+            resolve();
+          })
+        })
+      });
+    });
+    */
+    
     it('an image can be created', ()=> {
-      const key = uuid();
       const uris = {};
-      uris[key] = smiley;
+      const key1 = uuid();
+      const key2 = uuid();
+      const key3 = uuid();
+      uris[key1] = smiley;
+      uris[key2] = frowny;
+      uris[key3] = smiley;
       const image = Image.build({ uris });
-      const imageUri = `https://s3.amazonaws.com/${process.env.BUCKET}/${key}`;
+      const imageUri = (key)=> `https://s3.amazonaws.com/${process.env.BUCKET}/${key}`;
       return image.save()
         .then( image => {
-          expect(image.uris[key]).to.equal(imageUri)
+          expect(image.uris[key1]).to.equal(imageUri(key1))
+          expect(image.uris[key2]).to.equal(imageUri(key2))
+          expect(image.uris[key3]).to.equal(imageUri(key3))
+          const obj = {};
+          obj[key1] = null;
+          image.uris = obj;
+          return image.save();
+        })
+        .then( image => {
+          expect(image.uris[key1]).to.not.be.ok;
+          expect(image.uris[key2]).to.equal(imageUri(key2))
+          expect(image.uris[key3]).to.equal(imageUri(key3))
         });
     });
   });
